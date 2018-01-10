d6133a69bb1e3ff2587796644b502f81972034ea
Matt Payne <Payne@MattPayne.org>
2018-01-10 14:32:37 +0000

    Ex14: copy files from basic

---
+++ dog-panel/dog-panel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl : `basic.component.html`,
+    styleUrls: ['basic.component.css']
+})
+export class BasicComponent { 
+}
